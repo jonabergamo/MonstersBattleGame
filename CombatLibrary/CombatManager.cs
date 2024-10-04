749335d7d8459bacf6f87c9a19c064512275befa
GtOliv3r <123522878+GtOliv3r@users.noreply.github.com>
2024-10-04 19:54:05 -0300

    feat: add battle simulation

--- CombatLibrary/CombatManager.cs
+++ CombatLibrary/CombatManager.cs
@@ -18,5 +18,32 @@ namespace CombatLibrary
         {
             strategy.Execute(attacker, defender);
         }
+
+        // Método para realizar uma ação de ataque
+        public void AttackAction(Monster attacker, Monster defender)
+        {
+            SetStrategy(new AttackStrategy());
+            ExecuteAction(attacker, defender);
+        }
+
+        // Método para realizar uma ação de defesa
+        public void DefendAction(Monster defender)
+        {
+            SetStrategy(new DefendStrategy());
+            ExecuteAction(defender, defender);  // O defensor está defendendo a si mesmo
+        }
+
+        // Método para realizar uma habilidade especial (usada pelo dragão)
+        public void SpecialAbilityAction(Monster attacker, Monster defender)
+        {
+            SetStrategy(new DragonSpecialAbilityStrategy());
+            ExecuteAction(attacker, defender);
+        }
+
+        public void ShowMonstersHealth(Monster monster1, Monster monster2)
+        {
+            Console.WriteLine($"{monster1.Name} possui {monster1.Health} pontos de vida");
+            Console.WriteLine($"{monster2.Name} possui {monster2.Health} pontos de vida");
+        }
     }
 }
---
+++ CombatLibrary/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using CombatLibrary;
+using CoreLibrary.Entities;
+
+// Criando dois monstros
+Robot robot = new Robot()
+{
+    Name = "Cid",
+    Health = 200,
+    Attack = 25,
+    Defense = 15
+};
+Dragon dragon = new Dragon()
+{
+    Name = "dragonauta",
+    Health = 300,
+    Attack = 40,
+    Defense = 20
+};
+
+// Criando o gerenciador de combate
+CombatManager combatManager = new CombatManager();
+
+// Realizando um ataque
+combatManager.AttackAction(robot, dragon);  // Cid ataca Dragonauta
+
+// Realizando uma defesa
+combatManager.DefendAction(dragon);  // Dragonauta se defende
+
+// Usando uma habilidade especial
+combatManager.SpecialAbilityAction(dragon, robot);
+
+//Mostra os pontos de vida atual de cada monstro
+combatManager.ShowMonstersHealth(dragon, robot);
+
